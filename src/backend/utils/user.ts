006ae5cb0700d57437046bd3826b8bd0915ecc24
Mahdi <mahdiabdolkarimi073@gmail.com>
2026-09-26 11:15:59 +0330

    first commit

--- src/backend/utils/user.ts
+++ src/backend/utils/user.ts
@@ -26,6 +26,8 @@ export async function getUser(token: string | NextRequest) {
   }
   const user = (await prisma.user.findUnique({ where: { token: token+"" } }));
 
+  if (!user) return null;
+
   return ssrOptimize({
     ...user,
     token: "****",
